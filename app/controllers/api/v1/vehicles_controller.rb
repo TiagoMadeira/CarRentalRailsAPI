6501826f70e1ca05f9b8ada12c0652a07edcdbd2
TiagoMadeira <tiagopmadeira@gmail.com>
2025-01-28 17:01:21 +0000

    update vehicles controllers

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -46,6 +46,7 @@ class Api::V1::VehiclesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def vehicle_params
-      params.require(:vehicle).permit(:brand, :model, :category, :transmission, :type, :cost, :capacity)
+      params.require(:vehicle).expect(:id, :brand, :model)
+                              .permit(:category, :transmission, :vehicle_type, :cost, :capacity)
     end
 end
