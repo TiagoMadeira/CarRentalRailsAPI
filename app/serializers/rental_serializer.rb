ac3800741f34d0c1dea242b40fb771650b640007
TiagoMadeira <tiagopmadeira@gmail.com>
2025-01-30 17:42:25 +0000

    added total rental const onto rental serializer

--- app/serializers/rental_serializer.rb
+++ app/serializers/rental_serializer.rb
@@ -1,5 +1,5 @@
 class RentalSerializer < ActiveModel::Serializer
   has_one :blocked_date
-  
-  attributes :id, :vehicle_id, :user_id, :rental_state, :blocked_date
+
+  attributes :id, :vehicle_id, :user_id, :rental_state, :total_rental_cost, :blocked_date
 end
